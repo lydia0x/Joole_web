066559a667fa3eb7fce6f4885543e8fc095e7ace
lydia0x <xwzheng@ucdavis.edu>
2020-02-18 08:24:16 -0500

    done

--- src/containers/comparator.js
+++ src/containers/comparator.js
@@ -1,6 +1,121 @@
-import React from 'react';
+// import React from 'react';
 
 
-export default  function Comparator() {
-    return <h2>Comparator</h2>;
+// export default  function Comparator() {
+//     return <h2>Comparator</h2>;
+// }
+
+import React, { Component, useState } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions/auth';
+import Header from '../components/Header';
+import Product from './product';
+
+
+import styles from './comparator.module.css';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+
+
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import Link from '@material-ui/core/Link';
+
+
+
+// export default  function Detail() {
+//     return <h2>Detail</h2>;
+// }
+
+class Comparator extends Component {
+  
+
+
+  render() {
+    return (
+      <div>
+        <Header></Header>
+        <div>
+          <Container className={styles.containers}>
+            <Row className={styles.row_t}> 
+              <Col
+                className={styles.col_right}
+                >
+
+                <Container className={styles.container_right}>
+                  <Row className={styles.row1_r}>
+                    <Breadcrumbs aria-label="breadcrumb">
+                      <Link color="rgb(52, 92, 115)"  href="/">
+                        Mechanical
+                      </Link>
+                      <Link color="rgb(52, 92, 115)"  href="/project">
+                        HVAC Fans
+                      </Link>
+                      <Typography color="inherit">Comparator</Typography>
+                    </Breadcrumbs>
+                  </Row>
+
+                  <Row className={styles.row2_r}>
+                    <Container className={styles.container_row2_r}>
+                    {this.props.fanArr && this.props.fanArr.map((object, i) => {
+                        return (
+                          <div className={styles.fan_div}>
+                            <div className={styles.fan_class1}>
+                              <img className={styles.fan_image} src={object.fan_image}></img>
+                              <p>{object.manufacturer}</p>
+                              <p>{object.series}</p>
+                              <p>{object.model}</p>
+                            </div>
+                            <div className={styles.fan_class2}>
+                              <p>{object.airflow} CFM</p>
+                              <p>{object.power_max}W at max speed</p>
+                              <p>{object.sound_at_max_speed}dBA sound_at_max_speed</p>
+                              <p>{object.fan_sweep_diameter}"fan sweep diameter</p>
+                            </div >
+                            <div className={styles.fan_class3}>
+                              <p>Past specifications:</p>
+                              <p>{object.firm} firm </p>
+                              <p>{object.global} global</p>
+                              
+                            </div>
+                          </div>
+                        )
+                      })}
+                    </Container>
+                  </Row>
+                </Container>
+
+              </Col>
+            </Row>
+
+          </Container>
+        </div>
+
+      </div>)
+      ;
+  };
 }
+
+
+const mapStateToProps = state => {
+  return {
+    loading: state.auth.loading,
+    error: state.auth.error,
+    isAuthenticated: state.auth.token !== null,
+
+    fanArr: state.auth.fanArr
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    setFansArr: (fanArr) => dispatch(actions.setFansArr(fanArr))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Comparator);
+
--- src/containers/detail.js
+++ src/containers/detail.js
@@ -5,43 +5,163 @@
 //     return <h2>Detail</h2>;
 // }
 
-import React, { useState } from 'react';
-
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Toast from 'react-bootstrap/Toast';
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-
+import React, { Component, useState } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions/userInfo';
+import Header from '../components/Header';
+import Product from './product';
 
 
-const ExampleToast = ({ children }) => {
-  const [show, toggleShow] = useState(true);
+import styles from './detail.module.css';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
-  return (
-    <>
-      {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
-      <Toast show={show} onClose={() => toggleShow(false)}>
-        <Toast.Header>
-          <strong className="mr-auto">React-Bootstrap</strong>
-        </Toast.Header>
-        <Toast.Body>{children}</Toast.Body>
-      </Toast>
-    </>
-  );
+
+
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import Link from '@material-ui/core/Link';
+
+import { Tabs, Tab, Panel } from '@bumaga/tabs'
+import documentation from '../assets/documentation.png';
+
+
+class Detail extends Component {
+
+  render() {
+    return (
+      <div>
+        <Header></Header>
+        <div>
+          <Container className={styles.containers}>
+            <Row className={styles.row_t}>
+              <Col
+                className={styles.col_right}
+              >
+
+                <Container className={styles.container_right}>
+                  <Row className={styles.row1_r}>
+                    <Breadcrumbs aria-label="breadcrumb">
+                      <Link color="rgb(52, 92, 115)" href="/">
+                        Mechanical
+                      </Link>
+                      <Typography color="inherit">HVAC Fans</Typography>
+                    </Breadcrumbs>
+                  </Row>
+
+                  <Row className={styles.row2_r}>
+
+
+                    <Container className={styles.container_row2_r}>
+                      {this.props.selectedFan && (
+                        <div className={styles.tab}>
+                          <Tabs>
+                            <div>
+                              <Tab><button className={styles.tab_button}>Product Summary</button></Tab>
+                              <Tab><button className={styles.tab_button}>Product Details</button></Tab>
+                              <Tab><button className={styles.tab_button}>Product Documentation</button></Tab>
+                              <Tab><button className={styles.tab_button}>Contact</button></Tab>
+                            </div>
+
+                            <Panel name="Product Summary">
+                              <div className={styles.fan_div}>
+                                <div >
+                                  <p className={styles.title}>Product Summary</p>
+                                  <img className={styles.fan_image} src={this.props.selectedFan.fan_image}></img>
+                                  <p className={styles.sub_title}>DESCRIPTION:</p>
+                                  <p>Manufacturer: {this.props.selectedFan.manufacturer}</p>
+                                  <p>Series: {this.props.selectedFan.series}</p>
+                                  <p>Model: {this.props.selectedFan.model}</p>
+                                </div>
+                                <div >
+                                  <p className={styles.sub_title}>TYPE:</p>
+                                  <p>Use Type: {this.props.selectedFan.use_type}</p>
+                                  <p>Application: {this.props.selectedFan.application}</p>
+                                  <p>Mounting Location: {this.props.selectedFan.mounting_location}</p>
+                                  <p>Accessories: {this.props.selectedFan.accessories}</p>
+                                  <p>Model year: {this.props.selectedFan.model_year}</p>
+                                </div>
+
+                              </div>
+
+                            </Panel>
+                            <Panel name="Product Details">
+                              <div className={styles.fan_div}>
+                                <p className={styles.title}>Product Details</p>
+                                <p className={styles.sub_title}>SERIES INFORMATION:</p>
+                                <p>{this.props.selectedFan.detail}</p>
+
+
+                              </div>
+                            </Panel>
+                            <Panel name="Product Documentation">
+                              <div className={styles.fan_div}>
+                                <p className={styles.title}>Product Documentation</p>
+                                <img className={styles.documentation_image} src={documentation}></img>
+
+                              </div>
+                            </Panel>
+                            <Panel name="Product Contact">
+                              <div className={styles.fan_div}>
+                                <div >
+                                  <p className={styles.title}>Contact</p>
+                                  <p className={styles.sub_title}>SALES REPRESENTATIVE:</p>
+                                  <p>Name: {this.props.selectedFan.contact_s_name}</p>
+                                  <p>Phone: {this.props.selectedFan.contact_s_phone}</p>
+                                  <p>Email: {this.props.selectedFan.contact_s_email}</p>
+                                  <p>Web: {this.props.selectedFan.contact_s_web}</p>
+                                </div>
+                                <div >
+                                  <p className={styles.sub_title}>MANUFACTURER:</p>
+                                  <p>Department: {this.props.selectedFan.contact_m_department}</p>
+                                  <p>Phone: {this.props.selectedFan.contact_m_phone}</p>
+                                  <p>Email: {this.props.selectedFan.contact_m_email}</p>
+                                  <p>Web: {this.props.selectedFan.contact_m_web}</p>
+                                  
+                                </div>
+
+                              </div>
+                            </Panel>
+                          </Tabs>
+                        </div>
+                      )}
+
+
+                    </Container>
+                  </Row>
+                </Container>
+
+              </Col>
+            </Row>
+
+          </Container>
+        </div>
+
+      </div>)
+      ;
+  };
+}
+
+
+const mapStateToProps = state => {
+  return {
+    loading: state.auth.loading,
+    error: state.auth.error,
+    isAuthenticated: state.auth.token !== null,
+
+
+    selectedFan: state.auth.selectedFan
+  };
 };
 
-const Detail = () => (
-  <Container className="p-3">
-    <Jumbotron>
-      <h1 className="header">Welcome To React-Bootstrap</h1>
-      <ExampleToast className="toast">
-        We now have Toasts
-        <span role="img" aria-label="tada">
-          🎉
-        </span>
-      </ExampleToast>
-    </Jumbotron>
-  </Container>
-);
+const mapDispatchToProps = dispatch => {
+  return {
+    getHomePage: token => dispatch(actions.getHomePage(token))
+  };
+};
 
-export default Detail;
+export default connect(mapStateToProps, mapDispatchToProps)(Detail);
+
